913f4c0ddc245ce01469ede07dd1052972e64eef
skipper587 <45574121+skipper587@users.noreply.github.com>
2020-05-31 09:20:30 -0700

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+import discord
+
+from discord.ext.commands import Bot
+from discord.ext import commands
+from discord.utils import get
+
+import os
+import glob
+import datetime
+
+## -- Variables
+
+kelutralBot = commands.Bot(command_prefix="!")
+
+## -- Functions
+
+def getStats(date):
+    joins1 = 0
+    leaves1 = 0
+    rds1 = 0
+    
+    fileList = []
+    for file in glob.glob('join_data/'+ date + '*.tsk'):
+        fileList.append(file)
+    for file in fileList:
+        if os.path.exists(file):
+            fh = open(file, 'r')
+            joins = fh.read()
+            joins1 = joins1 + int(joins)
+            fh.close()
+        else:
+            joins = joins + 0
+    fileList = []
+    for file in glob.glob('leave_data/'+ date + '*.tsk'):
+        fileList.append(file)
+    for file in fileList:
+        if os.path.exists(file):
+            fh = open(file, 'r')
+            leaves = fh.read()
+            leaves1 = int(leaves) + leaves1
+            fh.close()
+        else:
+            leaves = leaves + 0
+    fileList = []
+    for file in glob.glob('rds/'+ date + '*.tsk'):
+        fileList.append(file)
+    for file in fileList:
+        if os.path.exists(file):
+            fh = open(file, 'r')
+            rds = fh.read()
+            rds1 = int(rds) + rds1
+            fh.close()
+        else:
+            rds = rds + 0
+            
+    return joins1, leaves1, rds1
+
+def outputCheck(user):
+    fileName = 'users/' + str(user.id) + '.tsk'
+
+    if not os.path.exists(fileName):
+        return "English"
+    else:
+        # Determines Language Output
+        fh = open(fileName, 'r')
+        content = fh.readlines()
+        fh.close()
+        lang = content[2].strip()
+        return lang
+
+def checkProfile(user, *args):
+    fileName = 'users/' + str(user.id) + '.tsk'
+    lines = ['Messages', 'User Name', 'Language Preference']
+    indexList = []
+    output = []
+    i = 0
+    line = 0
+    
+    if os.path.exists(fileName):
+        fh = open(fileName, 'r')
+        content = fh.readlines()
+        fh.close()
+
+        args = list(args)
+
+        while i < len(args):
+            indexList.append(lines.index(args[i]))
+            i += 1
+
+        indexList.sort()
+
+        while line < len(indexList):
+            output.insert(int(indexList[line]),content[int(indexList[line])].strip())
+            line += 1
+            
+        return output
+    else:
+        return output
+
+def checkPronouns(ctx, user):
+    pronounsRole = ['He/Him', 'She/Her', 'They/Them']
+    n = 0
+    while n < len(pronounsRole):
+        role = discord.utils.find(lambda r: r.name == pronounsRole[n], ctx.message.guild.roles)
+        if role in user.roles:
+            pronouns = pronounsRole[n]
+            break
+        else:
+            pronouns = "Unspecified"
+            n += 1
+    return pronouns
+            
+async def adminMsgs(ctx, bot):
+    user = ctx.message.author
+    rulesChannel = 715727832063410207
+    infoChannel = 715049168984473671
+    if user.top_role.name == "Olo'eyktan (Admin)":
+        path1 = 'files/rules1.txt'
+        path2 = 'files/rules2.txt'
+        path3 = 'files/rules3.txt'
+
+        fh = open(path1, 'r')
+        file1 = fh.read()
+        fh.close()
+
+        fh = open(path2, 'r')
+        file2 = fh.read()
+        fh.close()
+
+        fh = open(path3, 'r')
+        file3 = fh.read()
+        fh.close()
+
+        channel = bot.get_channel(rulesChannel)
+
+        await channel.send(file1)
+        await channel.send(file2)
+        await channel.send(file3)
+
+        # oloEyktan = 715044138864607334
+        # Eyktan = 715048580334878732
+        # Karyu = 715044889049563147
+        # Numeyu = 715044972436389890
+        # Eylan = 715048542468833321
+
+        # pre1 = "**Role Descriptions:**"
+        # pre2 = "{} : Administrators, responsible for upkeep of the Kelutral network of resources.".format(ctx.guild.get_role(oloEyktan).mention)
+        # pre3 = "{} : Moderators, responsible for monitoring content in the Kelutral Discord.".format(ctx.guild.get_role(Eyktan).mention)
+        # pre4 = "---"
+        # pre5 = "{} : Teacher, a capable, volunteer teacher who has shown knowledge and skill in the Na'vi Language".format(ctx.guild.get_role(Karyu).mention)
+        # pre6 = "---"
+        # pre7 = "{} : Someone who is learning the Na'vi language.".format(ctx.guild.get_role(Numeyu).mention)
+        # pre8 = "{} : Inactive or non-language-learning Avatar enthusiasts".format(ctx.guild.get_role(Eylan).mention)
+        # pre9 = "---"
+
+        # path4 = 'files/info.txt'
+
+        # fh = open(path4, 'r')
+        # file4 = fh.read()
+        # fh.close()
+
+        # channel = kelutralBot.get_channel(infoChannel)
+
+        # await channel.send(pre1)
+        # await channel.send(pre2)
+        # await channel.send(pre3)
+        # await channel.send(pre4)
+        # await channel.send(pre5)
+        # await channel.send(pre6)
+        # await channel.send(pre7)
+        # await channel.send(pre8)
+        # await channel.send(pre9)
+        # await channel.send(file4)
---
+++ namegen.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+import os
+import random
+
+## -- Variables
+
+## Na'vi Alphabet
+vowels = ["a","ä","e","i","ì","o","u","aw","ay","ew","ey"]
+vowelProbabilities = [10,10,10,10,10,10,10,2,2,2,2]
+consonants = ["'","f","h","k","kx","l","m","n","ng","p","px","r","s","t","tx","ts","v","w","y","z"]
+consonantProbabilities = [1,6,6,6,3,6,6,6,4,6,3,4,6,6,3,6,5,5,5,5]
+pseudovowels = ["ll","rr"]
+diphthongs = ["aw","ay","ew","ey"]
+
+preconsonants = ["f","s","ts"]
+onsets_withpre = ["k","kx","l","m","n","ng","p","px","r","t","tx","w","y"]
+onsetProbabilities = [5,2,5,5,5,4,5,2,4,5,2,3,3]
+codas = ["'","k","kx","l","m","n","ng","p","px","r","t","tx"]
+codaProbabilities = [50,8,3,8,8,8,3,8,3,8,8,3]
+
+# Language Rules #
+# A syllable may start with a vowel
+# A syllable may end with a vowel
+# A consonant may start a syllable
+# A consonant cluster comprised of f, s, or ts + p, t, k, px, tx, kx, m, n, ng, r, l, w, or y may start a syllable
+# Px, tx, kx, p, t, k, ', m, n, l, r, or ng may occur in syllable-final position
+# Ts, f, s, h, v, z, w, or y may not occur in syllable-final position
+# No consonant clusters in syllable-final position
+# A syllable with a pseudovowel must start with a consonant or consonant cluster and must not have a final consonant
+
+# Valid Syllables #
+# Just one vowel
+# Consonant and vowel
+# Consonant cluster and vowel
+# Vowel and coda
+# Consonant, vowel and coda
+# Consonant cluster, vowel and coda
+# Consonant and pseudovowel
+# Consonant cluster, pseudovowel
+
+## -- Functions
+
+## Syllable Creation Functions
+
+def ruleOne():
+    vowel = random.choices(vowels, weights=vowelProbabilities)
+    return vowel[0]
+
+def ruleTwo():
+    vowel = random.choices(vowels, weights=vowelProbabilities)
+    consonant = random.choices(consonants, weights=consonantProbabilities)
+    s = consonant[0] + vowel[0]
+    return s
+
+def ruleThree():
+    vowel = random.choices(vowels, weights=vowelProbabilities)
+    onset = random.choices(onsets_withpre, weights=onsetProbabilities)
+    s = preconsonants[random.randint(0,2)] + onset[0] + vowel[0]
+    return s
+
+def ruleFour():
+    vowel = random.choices(vowels, weights=vowelProbabilities)
+    s = vowel[0] + codas[random.randint(0,11)]
+    return s
+
+def ruleFive():
+    consonant = random.choices(consonants, weights=consonantProbabilities)
+    vowel = random.choices(vowels, weights=vowelProbabilities)
+    coda = random.choices(codas, weights=codaProbabilities)
+    s = consonant[0] + vowel[0] + coda[0]
+    return s
+
+def ruleSix():
+    vowel = random.choices(vowels, weights=vowelProbabilities)
+    onset = random.choices(onsets_withpre, weights=onsetProbabilities)
+    coda = random.choices(codas, weights=codaProbabilities)
+    s = preconsonants[random.randint(0,2)] + onset[0] + vowel[0] + coda[0]
+    return s
+
+def ruleSeven():
+    consonant = random.choices(consonants, weights=consonantProbabilities)
+    s = consonant[0] + pseudovowels[random.randint(0,1)]
+    return s
+
+def ruleEight():
+    onset = random.choices(onsets_withpre, weights=onsetProbabilities)
+    s = preconsonants[random.randint(0,2)] + onset[0] + pseudovowels[random.randint(0,1)]
+    return s
+	
+# Name Generation Function
+
+def nameGen(numSyllables):
+    names = []
+    name = ""
+    output = " "
+
+    i = int(numSyllables)
+
+    # Conditional Loop for Number of Syllables
+    while i>0:
+        syllables = [1, 2, 3, 4, 5, 6, 7, 8]
+        p = [50, 50, 7.5, 7.5, 7.5, 4, .5, .5]
+        rule = random.choices(syllables, weights = p)
+        rule = int(rule[0])
+        # rule = random.randint(0,7)
+        if rule == 1 and not i == 1:
+            name = name + ruleOne()
+            i-=1
+        elif rule == 2:
+            name = name + ruleTwo()
+            i-=1
+        elif rule == 3:
+            name = name + ruleThree()
+            i-=1
+        elif rule == 4:
+            name = name + ruleFour()
+            i-=1
+        elif rule == 5:
+            name = name + ruleFive()
+            i-=1
+        elif rule == 6:
+            name = name + ruleSix()
+            i-=1
+        elif rule == 7:
+            name = name + ruleSeven()
+            i-=1
+        else:
+            name = name + ruleEight()
+            i-=1
+
+    # Building the Output
+    name = name.replace("''", "'")
+    name = name.replace("kk","k")
+    name = name.replace("kxkx", "kx")
+    name = name.replace("mm", "m")
+    name = name.replace("nn", "n")
+    name = name.replace("ngng", "ng")
+    name = name.replace("pp", "p")
+    name = name.replace("pxpx", "px")
+    name = name.replace("tt", "t")
+    name = name.replace("txtx", "tx")
+    name = name.replace("yy","y")
+    name = name.replace("aa", "a")
+    name = name.replace("ää", "ä")
+    name = name.replace("ee", "e")
+    name = name.replace("ii", "i")
+    name = name.replace("ìì", "ì")
+    name = name.replace("oo", "o")
+    name = name.replace("uu", "u")
+    name = name.replace("lll","ll")
+    name = name.replace("rrr","rr")
+    name = name.capitalize()
+    
+    return name
