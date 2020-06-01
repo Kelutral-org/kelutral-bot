5cb92d7285f670270d83e760cba7b708f2c086cc
skipper587 <45574121+skipper587@users.noreply.github.com>
2020-05-31 20:29:33 -0700

    Create watcher.py

---
+++ watcher.py
@@ -0,0 +1,254 @@
+# -*- coding: utf-8 -*-
+import discord
+
+from discord.ext.commands import Bot
+from discord.ext import commands
+from discord.utils import get
+
+import random
+
+from datetime import datetime
+
+import asyncio
+import os
+import glob
+
+import admin
+
+## -- Functions
+
+## -- On Join
+async def onJoin(member, kelutralBot):
+    message_channel = 715052686487191583
+    channel = kelutralBot.get_channel(message_channel)
+    
+    embed=discord.Embed(color=0x07ca48)
+    embed.set_thumbnail(url=member.avatar_url)
+    embed.set_author(name="Member Joined",icon_url=member.avatar_url)
+    embed.add_field(name="New Member:", value=str(member.mention) + " " + str(member), inline=False)
+    embed.set_footer(text="ID: " + str(member.id) + " • Today at " + member.joined_at.strftime('%H:%M PST'))
+    await channel.send(embed=embed)
+    
+    # Checks the Blacklist for Blacklisted IDs
+    fileName = 'blacklist.txt'
+    count = 0
+    i = 0
+    fh = open(fileName, 'r')
+    lines = fh.readlines()
+    count = len(lines)
+    while i < count:
+        line = ''.join(lines[i])
+        line.strip()
+        if int(lines[i]) == member.id:
+            print("Found this user in the blacklist.")
+            if member.dm_channel is None:
+                   await member.create_dm()
+            await member.send("Sorry, you are forbidden from joining Kelutral.org.")
+            await member.ban()
+            print(member.name + " was banned.")
+            
+            target = member.guild.get_member(81105065955303424)
+            await target.send(name + " attempted to join Kelutral.org.")
+
+            break
+        i += 1
+    fh.close()
+    
+    # This will automatically give anyone the 'frapo' role when they join the server.
+    newRole = get(member.guild.roles, name="frapo")
+    await member.add_roles(newRole)
+    print("Gave " + member.name + " the role " + newRole.name + ".")
+    
+    # This will add the join to the count of joins for that day
+    today = datetime.now().strftime('%m-%d-%Y')
+    fileName = 'join_data/' + today + '.tsk'
+    if not os.path.exists(fileName):
+        fh = open(fileName, 'w')
+        fh.write('1')
+        fh.close()
+    else:
+        fh = open(fileName, 'r')
+        total = fh.read()
+        fh.close()
+        
+        fh = open(fileName, 'w')
+        total = int(total) + 1
+        fh.write(str(total))
+        fh.close()
+    
+    if member.dm_channel is None:
+        await member.create_dm()
+
+    embed=discord.Embed()
+    embed=discord.Embed(title="Welcome to the Kelutral.org Discord Server!", colour=0x6D326D)
+    embed.add_field(name="**Fwa ngal fìtsengit sunu ayoer!**", value="We are glad that you are here!\n\nWhen you get the chance, please read our rules and information channels to familiarize yourself with our code of conduct and roles. After that, please introduce yourself in #hell's-gate so that a moderator can assign you the proper role.\n\nIf you would like to personally assign your own pronouns, you can react to this message with \U00002640, \U00002642 or \U0001F308. Please be careful when making your selection, as changes can't be made without contacting a moderator.\n\n**Zola'u nìprrte' ulte siva ko!** Welcome, let's go!", inline=False)
+
+    message = await member.send(embed=embed)
+    emojis = ['\U00002642','\U00002640','\U0001F308']
+    pronounRole = ['He/Him','She/Her','They/Them']
+    for emoji in emojis:
+        await message.add_reaction(emoji)
+        
+    def check(reaction, user):
+        for emoji in emojis:
+            if str(reaction.emoji) == emoji:
+                foundEmoji = True
+                break
+            else:
+                foundEmoji = False
+        return user == member and foundEmoji
+
+    try:
+        reaction, user = await kelutralBot.wait_for('reaction_add', timeout=180.0, check=check)
+    except asyncio.TimeoutError:
+        await member.send("Window has passed to self-assign pronouns. Please DM a mod if you would still like to do so.")
+        target = member.guild.get_member(715296437335752714)
+        for emoji in emojis:
+            await message.remove_reaction(emoji, target)
+    else:
+        i = 0
+        while i < 3:
+            if str(reaction) == emojis[i]:
+                newRole = get(member.guild.roles, name=pronounRole[i])
+                await member.add_roles(newRole)
+                print("Assigned " + member.name + " " + pronounRole[i] + " pronouns.")
+                break
+            else:
+                i += 1
+                
+## -- On Leave
+async def onLeave(member, kelutralBot):
+    message_channel = 715052686487191583
+    channel = kelutralBot.get_channel(message_channel)
+    
+    embed=discord.Embed(color=0xe93535)
+    embed.set_thumbnail(url=member.avatar_url)
+    embed.set_author(name="Member Left",icon_url=member.avatar_url)
+    embed.add_field(name="Member:", value=str(member.mention) + " " + str(member), inline=False)
+    embed.set_footer(text="ID: " + str(member.id) + " • Today at " + datetime.now().strftime('%H:%M PST'))
+    await channel.send(embed=embed)
+    
+    # This will add the departure to the count of departures for that day
+    today = datetime.now().strftime('%m-%d-%Y')
+    checkJoin = member.joined_at.strftime('%m-%d-%Y')
+    
+    print(member.name + " left the server.")    
+    if checkJoin == today:
+        fileName = 'rds/' + today + '.tsk'
+        if not os.path.exists(fileName):
+            fh = open(fileName, 'w')
+            fh.write('1')
+            fh.close()
+        else:
+            fh = open(fileName, 'r')
+            total = fh.read()
+            fh.close()
+            
+            fh = open(fileName, 'w')
+            total = int(total) + 1
+            fh.write(str(total))
+            fh.close()
+    
+    fileName = 'leave_data/' + today + '.tsk'
+    if not os.path.exists(fileName):
+        fh = open(fileName, 'w')
+        fh.write('1')
+        fh.close()
+    else:
+        fh = open(fileName, 'r')
+        total = fh.read()
+        fh.close()
+        
+        fh = open(fileName, 'w')
+        total = int(total) + 1
+        fh.write(str(total))
+        fh.close()
+    
+    if member.dm_channel is None:
+        await member.create_dm()
+
+## -- On Message Delete
+async def onDelete(message, kelutralBot):
+    member = message.author
+    message_channel = 715052686487191583
+    channel = kelutralBot.get_channel(message_channel)
+    
+    embed=discord.Embed(color=0xe93535)
+    embed.set_thumbnail(url=member.avatar_url)
+    embed.set_author(name=str(member),icon_url=member.avatar_url)
+    embed.add_field(name="Message Deleted", value="**Message sent by " + str(member.mention) + " deleted in " + str(message.channel.mention) + "**\n" + str(message.content), inline=False)
+    embed.set_footer(text="ID: " + str(message.id) + " • Today at " + datetime.now().strftime('%H:%M PST'))
+    await channel.send(embed=embed)
+    
+## -- On Member Update
+async def onUpdate(before, after, kelutralBot):
+    message_channel = 715052686487191583
+    channel = kelutralBot.get_channel(message_channel)
+    nameList = []
+    i = 0
+    
+    listBeforeRoles = before.roles
+    listAfterRoles = after.roles
+    removedRoles = list(set(listBeforeRoles) - set(listAfterRoles))
+    addedRoles = list(set(listAfterRoles) - set(listBeforeRoles))
+    
+    if len(removedRoles) > 0:
+        for role in removedRoles:
+            nameList.append(removedRoles[i].name)
+            i += 1
+        embed=discord.Embed(color=0x5da9e9)
+        embed.set_thumbnail(url=after.avatar_url)
+        embed.set_author(name=str(after),icon_url=after.avatar_url)
+        embed.add_field(name="Member Updated", value=str(after.mention) + "** was removed from `" + ' '.join(nameList) + '`**', inline=False)
+        embed.set_footer(text="ID: " + str(after.id) + " • Today at " + datetime.now().strftime('%H:%M PST'))
+        
+        await channel.send(embed=embed)
+    elif len(addedRoles) > 0:
+        for role in addedRoles:
+            nameList.append(addedRoles[i].name)
+            i += 1
+        embed=discord.Embed(color=0x5da9e9)
+        embed.set_thumbnail(url=after.avatar_url)
+        embed.set_author(name=str(after),icon_url=after.avatar_url)
+        embed.add_field(name="Member Updated", value=str(after.mention) + "** was added to `" + ' '.join(nameList) + '`**', inline=False)
+        embed.set_footer(text="ID: " + str(after.id) + " • Today at " + datetime.now().strftime('%H:%M PST'))
+        
+        await channel.send(embed=embed)
+        
+    beforeName = before.display_name
+    afterName = after.display_name
+        
+    if beforeName != afterName:
+        embed=discord.Embed(color=0x5da9e9)
+        embed.set_thumbnail(url=after.avatar_url)
+        embed.set_author(name=str(after),icon_url=after.avatar_url)
+        embed.add_field(name="Member Updated", value='`' + str(beforeName) + '`** changed their name to `' + str(afterName) + '`**', inline=False)
+        embed.set_footer(text="ID: " + str(after.id) + " • Today at " + datetime.now().strftime('%H:%M PST'))
+        
+        await channel.send(embed=embed)
+
+## -- On Member Ban
+async def onBan(guild, user, kelutralBot):
+    message_channel = 715052686487191583
+    channel = kelutralBot.get_channel(message_channel)
+
+    embed=discord.Embed(color=0xe93535)
+    embed.set_thumbnail(url=user.avatar_url)
+    embed.set_author(name=str(user),icon_url=user.avatar_url)
+    embed.add_field(name="Member Banned", value=str(user.mention) + '** was banned**', inline=False)
+    embed.set_footer(text="ID: " + str(user.id) + " • Today at " + datetime.now().strftime('%H:%M PST'))
+    
+    await channel.send(embed=embed)
+    
+## -- On Member Unban
+async def onUnban(guild, user, kelutralBot):
+    message_channel = 715052686487191583
+    channel = kelutralBot.get_channel(message_channel)
+
+    embed=discord.Embed(color=0x5da9e9)
+    embed.set_thumbnail(url=user.avatar_url)
+    embed.set_author(name=str(user),icon_url=user.avatar_url)
+    embed.add_field(name="Member Banned", value=str(user.mention) + '** was unbanned**', inline=False)
+    embed.set_footer(text="ID: " + str(user.id) + " • Today at " + datetime.now().strftime('%H:%M PST'))
+    
+    await channel.send(embed=embed)
