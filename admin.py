906d581ae02cc913f088e5bd93633e942bff3857
skipper587 <45574121+skipper587@users.noreply.github.com>
2020-09-03 08:42:23 -0400

    Update admin.py

--- admin.py
+++ admin.py
@@ -7,11 +7,16 @@ from discord.utils import get
 
 import os
 import glob
-import datetime
+from datetime import datetime
 
 ## -- Variables
 
 kelutralBot = commands.Bot(command_prefix="!")
+
+## -- -- For Progression
+activeRoleNames = ["Koaktu","Tsamsiyu","Tsamsiyutsyìp","Eykyu","Ikran Makto","Taronyu","Taronyutsyìp","Hapxìtu","Hapxìtutsyìp","Zìma'uyu","Ketuwong","Frapo"]
+activeRoleTranslations = ["Veteran","Warrior","Trainee Warrior","Party Leader","Ikran Rider","Hunter","Trainee Hunter","Member","Trainee Member","Newcomer","Alien","Everyone"]
+activeRoleThresholds = [16384, 8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16,8]
 
 ## -- Functions
 
@@ -21,33 +26,33 @@ def getStats(date):
     rds1 = 0
     
     fileList = []
-    for file in glob.glob('join_data/'+ date + '*.tsk'):
+    for file in glob.glob('files/logs/join_data/'+ date + '*.tsk'):
         fileList.append(file)
     for file in fileList:
         if os.path.exists(file):
-            fh = open(file, 'r')
+            fh = open(file, 'r', encoding='utf-8')
             joins = fh.read()
             joins1 = joins1 + int(joins)
             fh.close()
         else:
             joins = joins + 0
     fileList = []
-    for file in glob.glob('leave_data/'+ date + '*.tsk'):
+    for file in glob.glob('files/logs/leave_data/'+ date + '*.tsk'):
         fileList.append(file)
     for file in fileList:
         if os.path.exists(file):
-            fh = open(file, 'r')
+            fh = open(file, 'r', encoding='utf-8')
             leaves = fh.read()
             leaves1 = int(leaves) + leaves1
             fh.close()
         else:
             leaves = leaves + 0
     fileList = []
-    for file in glob.glob('rds/'+ date + '*.tsk'):
+    for file in glob.glob('files/logs/rds/'+ date + '*.tsk'):
         fileList.append(file)
     for file in fileList:
         if os.path.exists(file):
-            fh = open(file, 'r')
+            fh = open(file, 'r', encoding='utf-8')
             rds = fh.read()
             rds1 = int(rds) + rds1
             fh.close()
@@ -57,20 +62,20 @@ def getStats(date):
     return joins1, leaves1, rds1
 
 def outputCheck(user):
-    fileName = 'users/' + str(user.id) + '.tsk'
+    fileName = 'files/users/' + str(user.id) + '.tsk'
 
     if not os.path.exists(fileName):
         return "English"
     else:
         # Determines Language Output
-        fh = open(fileName, 'r')
+        fh = open(fileName, 'r', encoding='utf-8')
         content = fh.readlines()
         fh.close()
         lang = content[2].strip()
         return lang
 
 def checkProfile(user, *args):
-    fileName = 'users/' + str(user.id) + '.tsk'
+    fileName = 'files/users/' + str(user.id) + '.tsk'
     lines = ['Messages', 'User Name', 'Language Preference']
     indexList = []
     output = []
@@ -78,23 +83,26 @@ def checkProfile(user, *args):
     line = 0
     
     if os.path.exists(fileName):
-        fh = open(fileName, 'r')
-        content = fh.readlines()
-        fh.close()
+        try:
+            fh = open(fileName, 'r', encoding='utf-8')
+            content = fh.readlines()
+            fh.close()
 
-        args = list(args)
+            args = list(args)
 
-        while i < len(args):
-            indexList.append(lines.index(args[i]))
-            i += 1
+            while i < len(args):
+                indexList.append(lines.index(args[i]))
+                i += 1
 
-        indexList.sort()
+            indexList.sort()
 
-        while line < len(indexList):
-            output.insert(int(indexList[line]),content[int(indexList[line])].strip())
-            line += 1
-            
-        return output
+            while line < len(indexList):
+                output.insert(int(indexList[line]),content[int(indexList[line])].strip())
+                line += 1
+                
+            return output
+        except:
+            print("ERROR: " + str(user.name) + ": " + str(user.id) + " has invalid encoding on their file!!")
     else:
         return output
 
@@ -110,7 +118,52 @@ def checkPronouns(ctx, user):
             pronouns = "Unspecified"
             n += 1
     return pronouns
-            
+                    
+async def roleUpdate(count, check, message, user):
+    i = 0
+    activeRoles = message.guild.roles
+    langCheck = outputCheck(message.author)
+    for roles in activeRoleNames:
+        if count >= activeRoleThresholds[i] and check.name != roles:
+            newRole = get(activeRoles, name=activeRoleNames[i])
+            oldRole = get(activeRoles, name=activeRoleNames[i+1])
+            await user.add_roles(newRole)
+            now = datetime.strftime(datetime.now(),'%H:%M')
+            print(now + ' -- Tìmìng txintìnit alu ' + newRole.name + ' tuteru alu ' + user.display_name + '.')
+            try:
+                if message.author.dm_channel is None:
+                    await message.author.create_dm()
+
+                if langCheck == "English":
+                    embed=discord.Embed()
+                    embed=discord.Embed(colour=0x1e3626)
+                    embed.add_field(name="New Rank Achieved on Kelutral.org", value="**Congratulations!** By participating in the community, you've leveled up! You're now a " + newRole.name + " (" + activeRoleTranslations[i] + ").", inline=False)
+                    # embed.set_thumbnail(ctx.guild.icon_url)
+                    
+                elif langCheck == "Na'vi":
+                    embed=discord.Embed()
+                    embed=discord.Embed(colour=0x1e3626)
+                    embed.add_field(name="Mipa txìntin lu ngaru mì Helutral.org", value="**Seykxel si nitram!** Nga slolu " + newRole.name + ".", inline=False)
+                    # embed.set_thumbnail(ctx.guild.icon_url)
+                    
+                await message.author.send(embed=embed)
+                
+                if check.name != "@everyone":
+                    await user.remove_roles(check)
+                    now = datetime.strftime(datetime.now(),'%H:%M')
+                    print(now + " -- 'olaku txintìnit alu " + check.name + " ta " + user.display_name + ".")
+                break
+            except:
+                now = datetime.strftime(datetime.now(),'%H:%M')
+                print(now + " -- Unable to DM this user.")
+                if check.name != "@everyone":
+                    await user.remove_roles(check)
+                    now = datetime.strftime(datetime.now(),'%H:%M')
+                    print(now + " -- 'olaku txintìnit alu " + check.name + " ta " + user.display_name + ".")
+        elif count >= activeRoleThresholds[i]:
+            break
+        i += 1
+        
 async def adminMsgs(ctx, bot):
     user = ctx.message.author
     rulesChannel = 715727832063410207
@@ -121,15 +174,15 @@ async def adminMsgs(ctx, bot):
         # path2 = 'files/rules2.txt'
         # path3 = 'files/rules3.txt'
 
-        # fh = open(path1, 'r')
+        # fh = open(path1, 'r', encoding='utf-8')
         # file1 = fh.read()
         # fh.close()
 
-        # fh = open(path2, 'r')
+        # fh = open(path2, 'r', encoding='utf-8')
         # file2 = fh.read()
         # fh.close()
 
-        # fh = open(path3, 'r')
+        # fh = open(path3, 'r', encoding='utf-8')
         # file3 = fh.read()
         # fh.close()
 
@@ -149,19 +202,24 @@ async def adminMsgs(ctx, bot):
         # pre2 = "{} : Administrators, responsible for upkeep of the Kelutral network of resources.".format(ctx.guild.get_role(oloEyktan).mention)
         # pre3 = "{} : Moderators, responsible for monitoring content in the Kelutral Discord.".format(ctx.guild.get_role(Eyktan).mention)
         # pre4 = "---"
-        # pre5 = "{} : Teacher, a capable, volunteer teacher who has shown knowledge and skill in the Na'vi Language".format(ctx.guild.get_role(Karyu).mention)
+        # pre5 = "{} : Teacher, a capable, volunteer teacher who has shown willingness to share their knowledge of the Na'vi Language with other learners.".format(ctx.guild.get_role(Karyu).mention)
         # pre6 = "---"
         # pre7 = "{} : Someone who is learning the Na'vi language.".format(ctx.guild.get_role(Numeyu).mention)
         # pre8 = "{} : Inactive or non-language-learning Avatar enthusiasts".format(ctx.guild.get_role(Eylan).mention)
         # pre9 = "---"
 
-        # path4 = 'files/info.txt'
+        # path1 = 'files/info1.txt'
+        # path2 = 'files/info2.txt'
 
-        # fh = open(path4, 'r')
-        # file4 = fh.read()
+        # fh = open(path1, 'r', encoding='utf-8')
+        # file1 = fh.read()
         # fh.close()
 
-        # channel = kelutralBot.get_channel(infoChannel)
+        # fh = open(path2, 'r', encoding='utf-8')
+        # file2 = fh.read()
+        # fh.close()
+
+        # channel = bot.get_channel(infoChannel)
 
         # await channel.send(pre1)
         # await channel.send(pre2)
@@ -172,7 +230,8 @@ async def adminMsgs(ctx, bot):
         # await channel.send(pre7)
         # await channel.send(pre8)
         # await channel.send(pre9)
-        # await channel.send(file4)
+        # await channel.send(file1)
+        # await channel.send(file2)
         
         path1 = 'files/resources1.txt'
         path2 = 'files/resources2.txt'
