5852280aac2b76aa50d0fd105285e6ef328d486d
skipper587 <45574121+skipper587@users.noreply.github.com>
2020-10-09 12:52:40 -0400

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -12,20 +12,18 @@ from datetime import datetime
 import asyncio
 import os
 import glob
+import json
 
+import config
 import admin
-
-## -- Variables
-ignoredRoles = ["Olo'eyktan (Admin)","Eyktan (Moderator)","Karyu (Teacher)","Numeyu (Learner)","'Eylan (Friend)","Tìkanu Atsleng (Bot)","He/Him","She/Her","They/Them","frapo"]
 
 ## -- Functions
 
 ## -- On Join
 async def onJoin(member, kelutralBot):
-    message_channel = 715052686487191583
-    channel = kelutralBot.get_channel(message_channel)
+    channel = kelutralBot.get_channel(config.modLog)
     
-    embed=discord.Embed(color=0x07ca48)
+    embed=discord.Embed(color=config.successColor)
     embed.set_thumbnail(url=member.avatar_url)
     embed.set_author(name="Member Joined",icon_url=member.avatar_url)
     embed.add_field(name="New Member:", value=str(member.mention) + " " + str(member), inline=False)
@@ -34,30 +32,25 @@ async def onJoin(member, kelutralBot):
     
     # Checks the Blacklist for Blacklisted IDs
     fileName = 'files/blacklist.txt'
-    count = 0
-    i = 0
-    fh = open(fileName, 'r', encoding='utf-8')
-    lines = fh.readlines()
-    count = len(lines)
-    while i < count:
-        line = ''.join(lines[i])
-        line.strip()
-        if int(lines[i]) == member.id:
-            now = datetime.strftime(datetime.now(),'%H:%M')
-            print(now + " -- Found this user in the blacklist.")
-            if member.dm_channel is None:
-                   await member.create_dm()
-            await member.send("Sorry, you are forbidden from joining Kelutral.org.")
-            await member.ban()
-            now = datetime.strftime(datetime.now(),'%H:%M')
-            print(now + " -- " + member.name + " was banned.")
-            
-            target = member.guild.get_member(81105065955303424)
-            await target.send(member.name + " attempted to join Kelutral.org.")
+    with open(fileName, 'r', encoding='utf-8') as fh:
+        lines = fh.readlines()
 
-            break
-        i += 1
-    fh.close()
+    setLines = set(lines)
+    
+    if str(member.id) in setLines:
+        now = datetime.strftime(datetime.now(),'%H:%M')
+        print(now + " -- Found this user in the blacklist.")
+        if member.dm_channel is None:
+               await member.create_dm()
+        await member.send("Sorry, you are forbidden from joining Kelutral.org.")
+        await member.ban()
+        now = datetime.strftime(datetime.now(),'%H:%M')
+        print(now + " -- " + member.name + " was banned.")
+        
+        target = member.guild.get_member(config.makoID)
+        await target.send(member.name + " attempted to join Kelutral.org.")
+        
+        return
     
     # This will automatically give anyone the 'frapo' role when they join the server.
     
@@ -70,25 +63,22 @@ async def onJoin(member, kelutralBot):
     today = datetime.now().strftime('%m-%d-%Y')
     fileName = 'files/logs/join_data/' + today + '.tsk'
     if not os.path.exists(fileName):
-        fh = open(fileName, 'w', encoding='utf-8')
-        fh.write('1')
-        fh.close()
+        with open(fileName, 'w', encoding='utf-8') as fh:
+            fh.write('1')
     else:
-        fh = open(fileName, 'r', encoding='utf-8')
-        total = fh.read()
-        fh.close()
+        with open(fileName, 'r', encoding='utf-8') as fh:
+            total = fh.read()
         
-        fh = open(fileName, 'w', encoding='utf-8')
         total = int(total) + 1
-        fh.write(str(total))
-        fh.close()
+        with open(fileName, 'w', encoding='utf-8') as fh:
+            fh.write(str(total))
     
     try:
         if member.dm_channel is None:
             await member.create_dm()
 
         embed=discord.Embed()
-        embed=discord.Embed(title="Welcome to the Kelutral.org Discord Server!", colour=0x6D326D)
+        embed=discord.Embed(title="Welcome to the Kelutral.org Discord Server!", colour=config.welcomeColor)
         embed.add_field(name="**Fwa ngal fìtsengit sunu ayoer!**", value="We are glad that you are here!\n\nWhen you get the chance, please read our rules and information channels to familiarize yourself with our code of conduct and roles. After that, please introduce yourself in #kaltxì so that a moderator can assign you the proper role.\n\nIf you would like to personally assign your own pronouns, you can react to this message with \U00002640, \U00002642 or \U0001F308. Please be careful when making your selection, as changes can't be made without contacting a moderator.\n\n**Zola'u nìprrte' ulte siva ko!** Welcome, let's go!", inline=False)
 
         message = await member.send(embed=embed)
@@ -110,7 +100,7 @@ async def onJoin(member, kelutralBot):
             reaction, user = await kelutralBot.wait_for('reaction_add', timeout=180.0, check=check)
         except asyncio.TimeoutError:
             await member.send("Window has passed to self-assign pronouns. Please DM a mod if you would still like to do so.")
-            target = member.guild.get_member(715296437335752714)
+            target = member.guild.get_member(config.botID)
             for emoji in emojis:
                 await message.remove_reaction(emoji, target)
         else:
@@ -130,10 +120,9 @@ async def onJoin(member, kelutralBot):
                 
 ## -- On Leave
 async def onLeave(member, kelutralBot):
-    message_channel = 715052686487191583
-    channel = kelutralBot.get_channel(message_channel)
+    channel = kelutralBot.get_channel(config.modLog)
     
-    embed=discord.Embed(color=0xe93535)
+    embed=discord.Embed(color=config.failColor)
     embed.set_thumbnail(url=member.avatar_url)
     embed.set_author(name="Member Left",icon_url=member.avatar_url)
     embed.add_field(name="Member:", value=str(member.mention) + " " + str(member), inline=False)
@@ -149,54 +138,42 @@ async def onLeave(member, kelutralBot):
     if checkJoin == today:
         fileName = 'files/logs/rds/' + today + '.tsk'
         if not os.path.exists(fileName):
-            fh = open(fileName, 'w', encoding='utf-8')
-            fh.write('1')
-            fh.close()
+            with open(fileName, 'w', encoding='utf-8') as fh:
+                fh.write('1')
         else:
-            fh = open(fileName, 'r', encoding='utf-8')
-            total = fh.read()
-            fh.close()
-            
-            fh = open(fileName, 'w', encoding='utf-8')
+            with open(fileName, 'r', encoding='utf-8') as fh:
+                total = fh.read()
             total = int(total) + 1
-            fh.write(str(total))
-            fh.close()
+            with open(fileName, 'w', encoding='utf-8') as fh:
+                fh.write(str(total))
     
     fileName = 'files/logs/leave_data/' + today + '.tsk'
     if not os.path.exists(fileName):
-        fh = open(fileName, 'w', encoding='utf-8')
-        fh.write('1')
-        fh.close()
+        with open(fileName, 'w', encoding='utf-8') as fh:
+            fh.write('1')
     else:
-        fh = open(fileName, 'r', encoding='utf-8')
-        total = fh.read()
-        fh.close()
-        
-        fh = open(fileName, 'w', encoding='utf-8')
-        total = int(total) + 1
-        fh.write(str(total))
-        fh.close()
-    
-    if member.dm_channel is None:
-        await member.create_dm()
+        with open(fileName, 'r', encoding='utf-8') as fh:
+            total = fh.read()
+        total = int(total) + 1        
+        with open(fileName, 'w', encoding='utf-8') as fh:
+            fh.write(str(total))
 
 ## -- On Message Delete
 async def onDelete(message, kelutralBot):
     member = message.author
-    message_channel = 715052686487191583
-    channel = kelutralBot.get_channel(message_channel)
+    channel = kelutralBot.get_channel(config.modLog)
     
-    embed=discord.Embed(color=0xe93535)
-    embed.set_thumbnail(url=member.avatar_url)
-    embed.set_author(name=str(member),icon_url=member.avatar_url)
-    embed.add_field(name="Message Deleted", value="**Message sent by " + str(member.mention) + " deleted in " + str(message.channel.mention) + "**\n" + str(message.content), inline=False)
-    embed.set_footer(text="ID: " + str(message.id) + " • Today at " + datetime.now().strftime('%H:%M EST'))
-    await channel.send(embed=embed)
+    if message.author.top_role.id != config.botRoleID:  
+        embed=discord.Embed(color=config.failColor)
+        embed.set_thumbnail(url=member.avatar_url)
+        embed.set_author(name=str(member),icon_url=member.avatar_url)
+        embed.add_field(name="Message Deleted", value="**Message sent by " + str(member.mention) + " deleted in " + str(message.channel.mention) + "**\n" + str(message.content), inline=False)
+        embed.set_footer(text="ID: " + str(message.id) + " • Today at " + datetime.now().strftime('%H:%M EST'))
+        await channel.send(embed=embed)
     
 ## -- On Member Update
 async def onUpdate(before, after, kelutralBot):
-    message_channel = 715052686487191583
-    channel = kelutralBot.get_channel(message_channel)
+    channel = kelutralBot.get_channel(config.modLog)
     nameList = []
     i = 0
     
@@ -209,7 +186,7 @@ async def onUpdate(before, after, kelutralBot):
         for role in removedRoles:
             nameList.append(removedRoles[i].name)
             i += 1
-        embed=discord.Embed(color=0x5da9e9)
+        embed=discord.Embed(color=config.reportColor)
         embed.set_thumbnail(url=after.avatar_url)
         embed.set_author(name=str(after),icon_url=after.avatar_url)
         embed.add_field(name="Member Updated", value=str(after.mention) + "** was removed from `" + ' '.join(nameList) + '`**', inline=False)
@@ -220,7 +197,7 @@ async def onUpdate(before, after, kelutralBot):
         for role in addedRoles:
             nameList.append(addedRoles[i].name)
             i += 1
-        embed=discord.Embed(color=0x5da9e9)
+        embed=discord.Embed(color=config.reportColor)
         embed.set_thumbnail(url=after.avatar_url)
         embed.set_author(name=str(after),icon_url=after.avatar_url)
         embed.add_field(name="Member Updated", value=str(after.mention) + "** was added to `" + ' '.join(nameList) + '`**', inline=False)
@@ -232,7 +209,7 @@ async def onUpdate(before, after, kelutralBot):
     afterName = after.display_name
         
     if beforeName != afterName:
-        embed=discord.Embed(color=0x5da9e9)
+        embed=discord.Embed(color=config.reportColor)
         embed.set_thumbnail(url=after.avatar_url)
         embed.set_author(name=str(after),icon_url=after.avatar_url)
         embed.add_field(name="Member Updated", value='`' + str(beforeName) + '`** changed their name to `' + str(afterName) + '`**', inline=False)
@@ -242,10 +219,9 @@ async def onUpdate(before, after, kelutralBot):
 
 ## -- On Member Ban
 async def onBan(guild, user, kelutralBot):
-    message_channel = 715052686487191583
-    channel = kelutralBot.get_channel(message_channel)
+    channel = kelutralBot.get_channel(config.modLog)
 
-    embed=discord.Embed(color=0xe93535)
+    embed=discord.Embed(color=config.failColor)
     embed.set_thumbnail(url=user.avatar_url)
     embed.set_author(name=str(user),icon_url=user.avatar_url)
     embed.add_field(name="Member Banned", value=str(user.mention) + '** was banned**', inline=False)
@@ -255,10 +231,10 @@ async def onBan(guild, user, kelutralBot):
     
 ## -- On Member Unban
 async def onUnban(guild, user, kelutralBot):
-    message_channel = 715052686487191583
-    channel = kelutralBot.get_channel(message_channel)
 
-    embed=discord.Embed(color=0x5da9e9)
+    channel = kelutralBot.get_channel(config.modLog)
+
+    embed=discord.Embed(color=config.reportColor)
     embed.set_thumbnail(url=user.avatar_url)
     embed.set_author(name=str(user),icon_url=user.avatar_url)
     embed.add_field(name="Member Banned", value=str(user.mention) + '** was unbanned**', inline=False)
@@ -271,42 +247,50 @@ async def onMessage(message, kelutralBot):
     user = message.author
     now = datetime.strftime(datetime.now(),'%H:%M')
     print(now + " - Analyzing message from " + str(user))
+    
     # If message is in-server
     if message.guild:
-        if not message.content.startswith("!"):
+        if not message.content.startswith("!") or not message.content.startswith("?"):
             # If message is in guild and isn't from the bot.
-            if len(message.content) >= 5 and message.author.top_role.id != 715094486992027699:
+            if len(message.content) >= 5 and message.author.top_role.id != config.botRoleID:
                 currentRole = user.top_role
                 userRoles = user.roles
                 isMod = False
-                userMessageCount = 0
-                fileName = 'files/users/' + str(user.id) + '.tsk'
                 
                 ## Check if author.top_role is moderator.
-                if currentRole.name in ignoredRoles:
+                if currentRole.name in config.ignoredRoles:
                     isMod = True
 
                 ## Assigns correct role to currentRole if mod.
                 if isMod:
                     for role in userRoles:
-                        if role.name not in ignoredRoles:
+                        if role.name not in config.ignoredRoles:
                             currentRole = role
 
                 ## Updates the user profile.
-                if not os.path.exists(fileName):
-                    fh = open(fileName, 'w', encoding='utf-8')
-                    fh.write(str(userMessageCount + 1) + "\n")
-                    fh.write(user.name + "\n")
-                    fh.write("English")
-                    fh.close()
-                    content = str(userMessageCount + 1)
+                profile = admin.getProfile(user)
+                
+                if profile == None:
+                    pronouns = admin.checkPronouns(message, user)
+                    n = 0
+                    while n < len(config.activeRoleNames):
+                        role = discord.utils.find(lambda r: r.name == config.activeRoleNames[n], message.guild.roles)
+                        if role in user.roles:
+                            activeRole = config.activeRoleNames[n]
+                            break
+                        else:
+                            activeRole = "Error"
+                            n += 1
+                    activeRoleTranslation = config.activeRoleTranslations[n]
+                    newProfile = [user.id,1,user.name,"English",pronouns,activeRole,activeRoleTranslation,0]
+                    config.directory.append(newProfile)
+                    
+                    print(now + " -- Created new profile for " + user.name)
+                    with open(config.directoryFile, 'w', encoding='utf-8') as fh:
+                        json.dump(config.directory, fh)
                 else:
-                    content = admin.checkProfile(user, "Messages", "User Name", "Language Preference")
+                    profile[1] += 1
+                    with open(config.directoryFile, 'w', encoding='utf-8') as fh:
+                        json.dump(config.directory, fh)
 
-                    fh = open(fileName, "w", encoding='utf-8')
-                    fh.write(str(int(content[0]) + 1) + "\n")
-                    fh.write(content[1] + "\n")
-                    fh.write(content[2])
-                    fh.close()
-                        
-                await admin.roleUpdate(int(content[0]) + 1, currentRole, message, user)
+                await admin.roleUpdate(profile[1], currentRole, message, user)
